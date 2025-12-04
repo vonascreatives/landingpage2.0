43959d534c915bed60702475e85edfb65b037b40
Valiantic <stevenmadali17@gmail.com>
2025-12-04 21:56:29 +0800

    Modify: Update Navbar Logo

--- src/layouts/headers/header-five.tsx
+++ src/layouts/headers/header-five.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import logo from '@/assets/img/logo/logo.png';
-import logo_2 from '@/assets/img/logo/logo-white.png';
+import logo from '@/assets/img/logo/FRMWRKD_LOGO.png';
+import logo_2 from '@/assets/img/logo/FRMWRKD_LOGO.png';
 import MobileOffcanvasTwo from "@/components/offcanvas/mobile-offcanvas-2";
 
 export default function HeaderFive() {
